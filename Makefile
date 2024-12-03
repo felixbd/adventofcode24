582810d2c04af081b8e2dcc007a7c0300e0ffc91
Felix D <fdrees@uni-bremen.de>
2024-12-03 06:28:28 +0100

    REALLY FUCKING FIXED

---
+++ Makefile
@@ -0,0 +1,6 @@
+run:
+	ghc aoc24.hs
+	./aoc24
+
+c:
+	rm *.hi *.o aoc24
--- aoc24.hs
+++ aoc24.hs
@@ -1,4 +1,4 @@
-import Data.List (sort)
+import Data.List (sort, nub, tails)
 
 -- day 1 --------------------------------------------------------------------
 
@@ -19,28 +19,28 @@ similarityScore xs ys = sum [x * sum [1 | y <- ys, y == x] | x <- xs]
 -- day 2 --------------------------------------------------------------------
 
 -- [[int(g) for g in e.split()] for e in x.split("\n")]
+-- [list(map(lambda g: int(g), e.split())) for e in x.split("\n")[:-1]]
 
 d2xss :: [[Int]]
 d2xss = [[7, 6, 4, 2, 1], [1, 2, 7, 8, 9], [9, 7, 6, 2, 1],
          [1, 3, 2, 4, 5], [8, 6, 4, 4, 1], [1, 3, 6, 7, 9]]
 
-predicate :: Int -> Int -> Bool
-predicate a b = (a /= b) && if a > b then a - b <= 3 else b - a <= 3
-
-numSafeReports :: [[Int]] -> Int
-numSafeReports xss = sum [1 | e <- map isReportSafe xss, e]
+isSafeBasic :: [Int] -> Bool
+isSafeBasic levels =
+    allIncreasing || allDecreasing
   where
-    isReportSafe :: [Int] -> Bool
-    isReportSafe xs = (xs == sort xs || xs == reverse (sort xs)) &&
-      and [predicate t z | (t, z) <- zip xs (tail xs)]
+    diffs = zipWith (-) (tail levels) levels
+    allIncreasing = all (\d -> d >= 1 && d <= 3) diffs
+    allDecreasing = all (\d -> d <= -1 && d >= -3) diffs
 
-numSafeReportsV2 :: [[Int]] -> Int
-numSafeReportsV2 xss = sum [1 | e <- map isReportSafe xss, e]
+isSafeWithDampener :: [Int] -> Bool
+isSafeWithDampener levels
+    | isSafeBasic levels = True
+    | otherwise = any (isSafeBasic . removeAt) [0 .. length levels - 1]
   where
-    isReportSafe :: [Int] -> Bool
-    isReportSafe xs = (xs == sort xs || xs == reverse (sort xs)) &&  -- todo ...
-      sum [1 | j <- [predicate t z | (t, z) <- zip xs (tail xs)], not j] < 2
+    removeAt i = take i levels ++ drop (i + 1) levels
 
+---------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
@@ -49,5 +49,5 @@ main = do
   print $ similarityScore d1xs d1ys
 
   putStrLn "day 2"
-  print $ numSafeReports d2xss
-  print $ numSafeReportsV2 d2xss
+  print $ sum [1 | e <- map isSafeBasic d2xss, e]
+  print $ sum [1 | e <- map isSafeWithDampener d2xss, e]
